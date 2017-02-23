584d300db2314aaa358195ea70c237e1a4e53488
PetronelaCretu <petronela.cretu@gmail.com>
2017-02-23 13:59:30 +0100

    re-implementation setters, getters

--- EditImage.py
+++ EditImage.py
@@ -1,16 +1,15 @@
 '''
 Created on 10.02.2017
 @author: Petronela Cretu
-Python version 2.7
 '''
 
-
-from __future__ import print_function
 from PIL import Image, ImageDraw, ImageFont
 
-import os, sys
+import os
 
-class EditImage():
+
+class EditImage(object):
+    
     def __init__(self):
         self._image = None
         self._imageName = ''
@@ -18,51 +17,47 @@ class EditImage():
     @property
     def image(self):
         return self._image
-    
+#     
     @property
     def imageName(self):
-        return self.imageName
+        return self._imageName
     
     @image.setter
     def image(self, filePath):
-        self._image, self._imageName = self.openImage(filePath)
+        self._image = Image.open(filePath)
         
-        
-    def openImage(self, imgName):
-        try:
-            return (Image.open(imgName), imgName)
-        except IOError:
-            print(imgName + ' error opening file')
-
-
+    @imageName.setter
+    def imageName(self,image):
+        self._imageName = self.image.filename
+            
+   
     def displayImage(self, img):
         img.show()
         
 
-    def resizeImg(self, img, imgName, picSize = (640, 427)):
-        x, y =  img.size
+    def resizeImg(self, picSize = (640, 427)):
+        x, y = self.image.size
         if x < y:
             z, t = picSize
             picSize = (t, z)
-        img = img.resize(picSize)
-        print(imgName)
-        imgName = "D:\\" + imgName.split("\\")[-1]
+        self._image = self._image.resize(picSize)
+
+#         self.imageName = "D:\\" + self.imageName.split("\\")[-1]
+
+        self._imageName = self._imageName.split(".")[0] + str(picSize) + "." + self._imageName.split(".")[1]
+        self._image.save(self._imageName, "JPEG")
         
-        print(imgName)
-        imgName = imgName.split(".")[0] + str(picSize) + "." + imgName.split(".")[1]
-        img.save(imgName, "JPEG")
-        return img, imgName
 
     def mergeImages(self, img1, img2):
         newImg = Image.blend(img1, img2, 1.0)
         newImg.save(img1.name + 'signed', "jpeg")
     
-    def addTextToImage(self,img, name, text = None, color = (0,0,0), location = (0,0), size = 42):
-        draw = ImageDraw.Draw(img)
+    def addTextToImage(self, text = None, color = (0,0,0), location = (0,0), size = 42):
+        draw = ImageDraw.Draw(self.image)
         #ITCBLKAD.TTF
         fnt = ImageFont.truetype("C:/Windows/Fonts/PALSCRI.TTF", size)
-        draw.text(location, "Petronella", color, font=fnt)
-        img.save(name, 'JPEG')
+        draw.text(location, text, color, font=fnt)
+        self.image.save(self.imageName, 'JPEG')
 
 
 def importImages(directory):
@@ -70,7 +65,7 @@ def importImages(directory):
     for root, dirs, files in os.walk(directory):
         for file in files:
             if file.endswith(".jpg") or file.endswith(".jpeg"):
-    #                 print(os.path.join(root, file))
+    #                 print os.path.join(root, file)
                 listFilesPaths.append(os.path.join(root, file))
     return listFilesPaths
 
@@ -78,13 +73,12 @@ if __name__ == '__main__':
     ei = EditImage()
     images = importImages("D:\\imgs")
     for name in images:
-        img, name = ei.openImage(name)
-        img, name = ei.resizeImg(img, name)
-        x, y = img.size
-        ei.addTextToImage(img, name, location=(x-120, y-40))
+        ei.image = name
+        ei.imageName = ei.image
+        ei.resizeImg((600, 400))
+        x, y = ei.image.size
+        ei.addTextToImage(text = "Your Name", color = (3,3,3), location=(x-120, y-40), size = 20)
 #       ei.displayImage(img)
-    
-    
     
 
 
